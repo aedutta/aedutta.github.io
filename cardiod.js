65f2204944b83d4e4c52e74bcdcb5c6ec35a6f74
Ashmit Dutta <59835959+aedutta@users.noreply.github.com>
2023-02-27 18:24:49 -0500

    Update cardiod.js

--- cardiod.js
+++ cardiod.js
@@ -1,4 +1,4 @@
-//modified from coding train
+//modified from coding train https://thecodingtrain.com/challenges/133-time-tables-cardioid-visualization
 let r;
 let factor = 0;
 
@@ -16,15 +16,15 @@ function getVector(index, total) {
 
 function draw() {
   background(0);
-  const total = 200; //int(map(mouseX, 0, width, 0, 200));
-  factor += 0.015;
+  const total = 300; 
+  factor += 0.02;
   
   translate(width / 2, height / 2);
-  strokeWeight(2);
+  strokeWeight(3);
   noFill();
   ellipse(0, 0, r * 2);
 
-  strokeWeight(2);
+  strokeWeight(3);
   for (let i = 0; i < total; i++) {
     const a = getVector(i, total);
     const b = getVector(i * factor, total);
